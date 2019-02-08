8f1bf6a2741571494b3c80908245909975ed2981
Astral02 <vikas04522@gmail.com>
2019-02-08 13:21:03 -0500

    rename and update with configure port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var config = require('./config').config;
 
 app.get('/getTCC', function (req, res) {
    fs.readFile( __dirname + "/" + "CCinfo.json", 'utf8', function (err, data) {
@@ -16,8 +17,8 @@ app.post('/postPCC', function (req, res) {
    });
 })
 
-var server = app.listen(86, function () {
+var server = app.listen(config.port, function () {
    var host = server.address().address
    var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", host, config.port)
 })
